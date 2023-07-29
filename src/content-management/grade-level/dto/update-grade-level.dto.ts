fe7cbba00ddbaacc77422647c5cea5e9d6bdc30d
a.tejareh <a.tejareh@dibaware.local>
2023-07-29 14:46:03 +0330

    fix: fix some issues

--- src/content-management/grade-level/dto/update-grade-level.dto.ts
+++ src/content-management/grade-level/dto/update-grade-level.dto.ts
@@ -12,7 +12,7 @@ export class UpdateGradeLevelDto extends PartialType(CreateGradeLevelDto) {
     example: "second",
   })
   @IsNotEmpty({ message: "فیلد عنوان پایه تحصیلی است" })
-  @Length(4, 20, { message: "عنوان کارپایه تحصیلی باید بین ۴ تا ۲۰ حرف باشد" })
+  @Length(3, 20, { message: "عنوان کارپایه تحصیلی باید بین ۳ تا ۲۰ حرف باشد" })
   readonly title: string;
   readonly books?: Book[];
   readonly terms?: TermOfStudy[];
