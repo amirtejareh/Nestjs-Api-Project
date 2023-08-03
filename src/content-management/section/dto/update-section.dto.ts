5e2e4825cf14f9d670295f0a0069c18f25428f8a
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 11:12:40 +0330

    fix: update section dto

--- src/content-management/section/dto/update-section.dto.ts
+++ src/content-management/section/dto/update-section.dto.ts
@@ -4,16 +4,16 @@ import { GradeLevel } from "../../grade-level/entities/grade-level.entity";
 import { Book } from "../../book/entities/book.entity";
 import { CreateSectionDto } from "./create-section.dto";
 import { Chapter } from "../../chapter/entities/chapter.entity";
+
 export class UpdateSectionDto extends PartialType(CreateSectionDto) {
   readonly _id?: string;
   @ApiProperty({
-    description: "Title of the field of study",
-    example: "Math",
+    description: "Title of the section",
+    example: "section 1",
   })
-  @IsNotEmpty({ message: "فیلد عنوان رشته تحصیلی اجباری است" })
-  @Length(3, 20, { message: "عنوان رشته تحصیلی باید بین ۳ تا ۲۰ حرف باشد" })
+  @IsNotEmpty({ message: "فیلد عنوان بخش اجباری است" })
+  @Length(3, 20, { message: "عنوان بخش باید بین ۳ تا ۲۰ حرف باشد" })
   readonly title: string;
-
   readonly gradeLevels: GradeLevel[];
   readonly books: Book[];
   readonly chapters: Chapter[];
