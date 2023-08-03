c20af794d93f0a8a8b8c8caa947ed2e324aaff64
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 11:30:39 +0330

    feat: add repository section

--- src/content-management/section/section.repository.ts
+++ src/content-management/section/section.repository.ts
@@ -1,26 +1,105 @@
-import { Injectable } from "@nestjs/common";
+import {
+  Body,
+  ConflictException,
+  HttpStatus,
+  Injectable,
+  Param,
+  Res,
+} from "@nestjs/common";
 import { CreateSectionDto } from "./dto/create-section.dto";
 import { UpdateSectionDto } from "./dto/update-section.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Section } from "./entities/section.entity";
+import { Model } from "mongoose";
 
 @Injectable()
 export class SectionRepository {
-  create(createSectionDto: CreateSectionDto) {
-    return "This action adds a new section";
+  constructor(
+    @InjectModel(Section.name)
+    private readonly sectionModel: Model<Section>
+  ) {}
+
+  async findOneByTitle(title: string) {
+    return this.sectionModel.findOne({ title }).exec();
+  }
+  async create(@Res() res, @Body() createSectionDto: CreateSectionDto) {
+    try {
+      if (await this.findOneByTitle(createSectionDto.title)) {
+        throw new ConflictException("درج بخش تکراری امکان‌پذیر نمی‌باشد.");
+      }
+
+      const createSectionModel = await this.sectionModel.create(
+        createSectionDto
+      );
+      return res.status(200).json({
+        statusCode: 200,
+        message: "یک بخش با موفقیت ایجاد شد",
+        data: createSectionModel,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        statusCode: 500,
+        message: e.message,
+      });
+    }
   }
 
   findAll() {
-    return `This action returns all section`;
+    return this.sectionModel.find({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} section`;
+  findOne(@Param("id") id: string) {
+    return this.sectionModel.findOne({ _id: id });
   }
 
-  update(id: number, updateSectionDto: UpdateSectionDto) {
-    return `This action updates a #${id} section`;
+  async update(
+    @Res() res,
+    @Param("id") id: string,
+    @Body() updateSectionDto: UpdateSectionDto
+  ) {
+    try {
+      const updateSectionModel = await this.sectionModel.findOneAndUpdate(
+        { _id: id },
+        { $set: { ...updateSectionDto } },
+        { new: true }
+      );
+
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: "بخش مورد نظر با موفقیت بروزرسانی شد",
+        data: updateSectionModel,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: "مشکلی در بروزرسانی بخش مورد نظر به وجود آمده است",
+        error: error.message,
+      });
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} section`;
+  async remove(@Res() res, @Param("id") id: string) {
+    try {
+      const deleteSectionModel = await this.sectionModel.deleteOne({
+        _id: id,
+      });
+      if (!deleteSectionModel) {
+        return res.status(HttpStatus.NOT_FOUND).json({
+          statusCode: HttpStatus.NOT_FOUND,
+          message: "فصل مورد نظر پیدا نشد",
+        });
+      }
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: "فصل مورد نظر با موفقیت حذف شد",
+        data: deleteSectionModel,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: "مشکلی در حذف فصل مورد نظر به وجود آمده است",
+        error: error.message,
+      });
+    }
   }
 }
