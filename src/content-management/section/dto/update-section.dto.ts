635d0192b1ae7ddba6a2e8acbe7ada8141ebaa75
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 11:09:40 +0330

    feat: add update section

--- src/content-management/section/dto/update-section.dto.ts
+++ src/content-management/section/dto/update-section.dto.ts
@@ -1,4 +1,22 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSectionDto } from './create-section.dto';
+import { ApiProperty, PartialType } from "@nestjs/swagger";
+import { IsNotEmpty, Length } from "class-validator";
+import { GradeLevel } from "../../grade-level/entities/grade-level.entity";
+import { Book } from "../../book/entities/book.entity";
+import { CreateSectionDto } from "./create-section.dto";
+import { Chapter } from "../../chapter/entities/chapter.entity";
+export class UpdateSectionDto extends PartialType(CreateSectionDto) {
+  readonly _id?: string;
+  @ApiProperty({
+    description: "Title of the field of study",
+    example: "Math",
+  })
+  @IsNotEmpty({ message: "فیلد عنوان رشته تحصیلی اجباری است" })
+  @Length(3, 20, { message: "عنوان رشته تحصیلی باید بین ۳ تا ۲۰ حرف باشد" })
+  readonly title: string;
 
-export class UpdateSectionDto extends PartialType(CreateSectionDto) {}
+  readonly gradeLevels: GradeLevel[];
+  readonly books: Book[];
+  readonly chapters: Chapter[];
+  readonly createdAt?: Date;
+  readonly updatedAt?: Date;
+}
