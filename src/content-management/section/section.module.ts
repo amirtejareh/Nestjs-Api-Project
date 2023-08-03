14a8a8bc01175c8d5389b87a6ee8d351cd97fdb5
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 11:16:25 +0330

    feat: add section repository and jwt service to the module section

--- src/content-management/section/section.module.ts
+++ src/content-management/section/section.module.ts
@@ -3,12 +3,14 @@ import { SectionService } from "./section.service";
 import { SectionController } from "./section.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Section, SectionSchema } from "./entities/section.entity";
+import { SectionRepository } from "./section.repository";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Section.name, schema: SectionSchema }]),
   ],
   controllers: [SectionController],
-  providers: [SectionService],
+  providers: [SectionService, SectionRepository, JwtService],
 })
 export class SectionModule {}
