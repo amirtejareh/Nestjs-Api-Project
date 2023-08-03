5763ec92a846060de9bb4803467185bcc1bbf30b
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 11:14:26 +0330

    feat: add section module

--- src/content-management/section/section.module.ts
+++ src/content-management/section/section.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { SectionService } from './section.service';
-import { SectionController } from './section.controller';
+import { Module } from "@nestjs/common";
+import { SectionService } from "./section.service";
+import { SectionController } from "./section.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Section, SectionSchema } from "./entities/section.entity";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Section.name, schema: SectionSchema }]),
+  ],
   controllers: [SectionController],
-  providers: [SectionService]
+  providers: [SectionService],
 })
 export class SectionModule {}
