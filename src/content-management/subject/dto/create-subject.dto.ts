6b6fa6ea0013a48045f68df15d94eab7492c97e4
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 14:58:36 +0330

    feat: add create subject dto

--- src/content-management/subject/dto/create-subject.dto.ts
+++ src/content-management/subject/dto/create-subject.dto.ts
@@ -1,1 +1,21 @@
-export class CreateSubjectDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, Length } from "class-validator";
+import { GradeLevel } from "../../grade-level/entities/grade-level.entity";
+import { Book } from "../../book/entities/book.entity";
+import { TermOfStudy } from "../../term-of-study/entities/term-of-study.entity";
+
+export class CreateSubjectDto {
+  readonly _id?: string;
+  @ApiProperty({
+    description: "Title of the subject",
+    example: "Subject 1",
+  })
+  @IsNotEmpty({ message: "فیلد عنوان موضوع اجباری است" })
+  @Length(3, 20, { message: "عنوان موضوع باید بین ۳ تا ۲۰ حرف باشد" })
+  readonly title: string;
+  readonly gradeLevels: GradeLevel[];
+  readonly books: Book[];
+  readonly terms: TermOfStudy[];
+  readonly createdAt?: Date;
+  readonly updatedAt?: Date;
+}
