efd446de020e64b51a75675d51f912944aca46e5
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 11:57:19 +0330

    fix: remove csurf package deprecated

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { config } from "dotenv";
 import { ThrottleMiddleware } from "./middleware/throttle.middleware";
 import express from "express";
 import { join } from "path";
-import * as csurf from "csurf";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +20,7 @@ async function bootstrap() {
     .setVersion("0.1")
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup("api", app, document);
   await seederService.seed();
@@ -31,7 +31,6 @@ async function bootstrap() {
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   });
-  app.use(csurf());
   app.listen(3000);
 }
 
