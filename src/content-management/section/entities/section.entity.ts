d925eac5586c9c801e1c2d3728fb5ecdee1c56a9
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 11:03:39 +0330

    feat: add the entity section

--- src/content-management/section/entities/section.entity.ts
+++ src/content-management/section/entities/section.entity.ts
@@ -1,1 +1,29 @@
-export class Section {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Schema as mongooseSchema } from "mongoose";
+import { GradeLevel } from "../../grade-level/entities/grade-level.entity";
+import { Book } from "../../book/entities/book.entity";
+import { Chapter } from "../../chapter/entities/chapter.entity";
+
+export type SectionDocument = Section & Document;
+
+@Schema({ timestamps: true })
+export class Section {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: GradeLevel.name }],
+  })
+  gradeLevels: GradeLevel[];
+
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: Book.name }],
+  })
+  books: Book[];
+
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: Chapter.name }],
+  })
+  chapters: Chapter[];
+}
+export const SectionSchema = SchemaFactory.createForClass(Chapter);
