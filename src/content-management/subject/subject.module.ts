befb2bc9a7e75f57374abf42c5eaaf56a1a1f31a
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 15:29:21 +0330

    feat: add the module subject to the module app

--- src/content-management/subject/subject.module.ts
+++ src/content-management/subject/subject.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { SubjectService } from './subject.service';
-import { SubjectController } from './subject.controller';
+import { Module } from "@nestjs/common";
+import { SubjectService } from "./subject.service";
+import { SubjectController } from "./subject.controller";
+import { SubjectRepository } from "./subject.repository";
+import { JwtService } from "@nestjs/jwt";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Subject, SubjectSchema } from "./entities/subject.entity";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Subject.name, schema: SubjectSchema }]),
+  ],
   controllers: [SubjectController],
-  providers: [SubjectService]
+  providers: [SubjectService, SubjectRepository, JwtService],
 })
 export class SubjectModule {}
