1d614e9a61e698a4458077bd6b255f2b09958e06
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 15:20:59 +0330

    feat: add subject controller methods

--- src/content-management/subject/subject.controller.ts
+++ src/content-management/subject/subject.controller.ts
@@ -6,18 +6,27 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Res,
 } from "@nestjs/common";
 import { SubjectService } from "./subject.service";
 import { CreateSubjectDto } from "./dto/create-subject.dto";
 import { UpdateSubjectDto } from "./dto/update-subject.dto";
-
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { Roles } from "../../common/decorators/roles.decorator";
+import { RoleGuard } from "../../auth/guards/role.guard";
+import { AuthGuard } from "../../auth/guards/auth.guard";
+@ApiTags("Subject")
 @Controller("subject")
 export class SubjectController {
   constructor(private readonly subjectService: SubjectService) {}
 
   @Post()
-  create(@Body() createSubjectDto: CreateSubjectDto) {
-    return this.subjectService.create(createSubjectDto);
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles("SuperAdmin")
+  create(@Res() res, @Body() createSubjectDto: CreateSubjectDto) {
+    return this.subjectService.create(res, createSubjectDto);
   }
 
   @Get()
@@ -27,16 +36,26 @@ export class SubjectController {
 
   @Get(":id")
   findOne(@Param("id") id: string) {
-    return this.subjectService.findOne(+id);
+    return this.subjectService.findOne(id);
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() updateSubjectDto: UpdateSubjectDto) {
-    return this.subjectService.update(+id, updateSubjectDto);
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles("SuperAdmin")
+  update(
+    @Res() res,
+    @Param("id") id: string,
+    @Body() updateSubjectDto: UpdateSubjectDto
+  ) {
+    return this.subjectService.update(res, id, updateSubjectDto);
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.subjectService.remove(+id);
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard, RoleGuard)
+  @Roles("SuperAdmin")
+  remove(@Res() res, @Param("id") id: string) {
+    return this.subjectService.remove(res, id);
   }
 }
