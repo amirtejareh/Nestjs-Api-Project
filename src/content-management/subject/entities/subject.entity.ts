8d3e7563ccb856ad72faeb6a8bed81ebc70737f4
Amir Tejareh <amirtejareh@gmail.com>
2023-08-03 15:11:08 +0330

    feat: add term of study

--- src/content-management/subject/entities/subject.entity.ts
+++ src/content-management/subject/entities/subject.entity.ts
@@ -1,1 +1,28 @@
-export class Subject {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Schema as mongooseSchema } from "mongoose";
+import { GradeLevel } from "../../grade-level/entities/grade-level.entity";
+import { Book } from "../../book/entities/book.entity";
+import { TermOfStudy } from "../../term-of-study/entities/term-of-study.entity";
+
+export type SubjectDocument = Subject & Document;
+
+@Schema({ timestamps: true })
+export class Subject {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: GradeLevel.name }],
+  })
+  gradeLevels: GradeLevel[];
+
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: Book.name }],
+  })
+  books: Book[];
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: TermOfStudy.name }],
+  })
+  terms: TermOfStudy[];
+}
+export const SubjectSchema = SchemaFactory.createForClass(Subject);
