84247ec7a9ef1315f838f8be5729337a58bf582a
a.tejareh <a.tejareh@dibaware.local>
2023-08-07 16:56:28 +0330

    feat: add question resource

---
+++ src/exam-management/question/entities/question.entity.ts
@@ -0,0 +1,60 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Schema as mongooseSchema } from "mongoose";
+import { GradeLevel } from "../../../content-management/grade-level/entities/grade-level.entity";
+import { Book } from "../../../content-management/book/entities/book.entity";
+import { Section } from "../../../content-management/section/entities/section.entity";
+import { Subject } from "../../../content-management/subject/entities/subject.entity";
+import { Chapter } from "../../../content-management/chapter/entities/chapter.entity";
+
+export type QuestionDocument = Question & Document;
+
+@Schema({ timestamps: true })
+export class Question {
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: GradeLevel.name }],
+  })
+  gradeLevels: GradeLevel[];
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: Book.name }],
+  })
+  books: Book[];
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: Chapter.name }],
+  })
+  chapters: Chapter[];
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: Section.name }],
+  })
+  sections: Section[];
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: Subject.name }],
+  })
+  subjects: Subject[];
+
+  @Prop({ required: true, enum: ["hard", "easy", "average"] })
+  difficulty: string;
+  @Prop({
+    required: true,
+    enum: [
+      "conceptional",
+      "computational",
+      "trick",
+      "memorizational",
+      "challenging",
+    ],
+  })
+  type: string;
+
+  @Prop({ required: true })
+  question: string;
+
+  @Prop({ required: true })
+  correctAnswer: number;
+
+  @Prop({ required: true })
+  examNumber: number;
+  @Prop({ required: true })
+  questionNumber: number;
+}
+
+export const QuestionSchema = SchemaFactory.createForClass(Question);
