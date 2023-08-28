fea1bb2436ec89b5024b74e0a3a9a1870b1d9254
a.tejareh <a.tejareh@dibaware.local>
2023-08-28 17:01:31 +0330

    feat: add the property book reference to the entity book

--- src/content-management/book/entities/book.entity.ts
+++ src/content-management/book/entities/book.entity.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Schema as mongooseSchema } from "mongoose";
 import { GradeLevel } from "../../grade-level/entities/grade-level.entity";
+import { BookReference } from "../../book-reference/entities/book-reference.entity";
 
 export type BookDocument = Book & Document;
 
@@ -15,6 +16,11 @@ export class Book {
 
   @Prop({ required: false })
   image: string;
+
+  @Prop({
+    type: [{ type: mongooseSchema.Types.ObjectId, ref: BookReference.name }],
+  })
+  bookReference: BookReference[];
 
   @Prop({
     type: [{ type: mongooseSchema.Types.ObjectId, ref: GradeLevel.name }],
